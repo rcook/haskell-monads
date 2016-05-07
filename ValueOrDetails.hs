4cb00e236fc02fd6c00a0bdade992e0dfcd1767f
Spiro Michaylov <spiro@michaylov.com>
2016-05-07 07:51:14 -0700

    foldM version of ValueOrDetails example

--- ValueOrDetails.hs
+++ ValueOrDetails.hs
@@ -1,4 +1,8 @@
 
+-- used only for standard operations on monads
+
+import Control.Monad
+
 -- a type constructor that allows representing two kinds of errors 
 
 data ValueOrDetails v =
@@ -14,9 +18,18 @@ instance Monad ValueOrDetails where
     (Value e) >>= f             = f e
     return                      = Value
 
+-- need a way to print the monad
+
+instance Show a => Show (ValueOrDetails a) where
+  show (Value v) = show v
+  show (TooLargeError s) = "Value too large: " ++ s
+  show (TooSmallError s) = "Value too small: " ++ s
+
 -- use it to get the max value of an integer list where
 -- values are required to be in the interval [l,u]
 -- if the list is empty we return l
+
+-- start with a recursive version based on do
 
 maxInRange        :: Integer -> Integer -> [Integer] -> ValueOrDetails Integer
 maxInRange l u values     = maxInRangeRec l u (return l) values
@@ -33,18 +46,27 @@ maxInRangeRec l u vod (x:xs) =
                rest <- maxInRangeRec l u (return nvod) xs
                return rest
 
--- need a way to print the result
+-- alternate version based on foldM
 
-instance Show a => Show (ValueOrDetails a) where
-  show (Value v) = show v
-  show (TooLargeError s) = "Value too large: " ++ s
-  show (TooSmallError s) = "Value too small: " ++ s
+f :: Integer -> Integer -> Integer -> Integer -> ValueOrDetails Integer
+f l u acc val = 
+  if val > u then (TooLargeError (show val)) 
+             else if val < l then (TooSmallError (show val))
+             else return (max val acc)
+
+maxInRangeF :: Integer -> Integer -> [Integer] -> ValueOrDetails Integer
+maxInRangeF l u values = foldM (f l u) l values
 
 l1 = [11,12,10,17,13]
 l2 = [11,5,10,17,2,13]
 l3 = [11,25,10,17,80,13]
 
 main = do
+         print "recursive version using do ..."
          print (maxInRange 10 20 l1)
 	 print (maxInRange 10 20 l2)
 	 print (maxInRange 10 20 l3)
+         print "foldM version ..."
+         print (maxInRangeF 10 20 l1)
+	 print (maxInRangeF 10 20 l2)
+	 print (maxInRangeF 10 20 l3)
