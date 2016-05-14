8a3e74ac127df9225f90e569fb74fcbfe710226e
Spiro Michaylov <spiro@michaylov.com>
2016-05-14 11:30:34 -0700

    First attempt at using State monad

--- StatefulSymbolGen.hs
+++ StatefulSymbolGen.hs
@@ -1,13 +1,46 @@
 
-symbolStep :: (Int,Int) -> (String,(Int,Int))
-symbolStep (c1,c2) = let sym = "sym_" ++ (show c1) ++ "_" ++ (show c2)
-                      in (sym, (c1,c2+1))
+import Control.Monad.State
 
+type SymbolCounter = (Int,Int)
 
+-- how do you initialize the state?
+initial :: SymbolCounter
+initial = (0,0)
+  
+-- how do you advance the state?
+advance :: SymbolCounter -> SymbolCounter
+advance (c1,c2) = (c1,c2+1)
 
-main = let preState = (2,3) 
-           (sym,postState) = symbolStep preState
-        in 
-           do
-             print sym
-             print postState
+next :: SymbolCounter -> SymbolCounter
+next (c1,c2) = (c1+1,0)
+                 
+-- how do you create a symbol?
+makeSymbol :: SymbolCounter -> String
+makeSymbol (c1,c2) = "sym_" ++ (show c1) ++ "_" ++ (show c2)
+
+-- operations using the State monad
+
+initsym :: State SymbolCounter ()
+initsym = state $ \p -> ((), initial)
+
+stateValue :: State SymbolCounter SymbolCounter
+stateValue = modify advance >> get
+
+ms :: State SymbolCounter String
+ms = state $ \p -> (makeSymbol p, advance p)
+
+roll :: State SymbolCounter ()
+roll = modify next
+  
+stuff :: State SymbolCounter String 
+stuff = do
+  a <- ms
+  b <- ms
+  roll
+  c <- ms
+  d <- ms
+  return d
+
+main = do
+  print (evalState stuff initial)
+  
